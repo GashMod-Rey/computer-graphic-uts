101aa0fa9097736aedad6b742dcd801ffdd9b9cf
GashMod-Rey <reynard.yaputra@student.umn.ac.id>
2022-10-19 18:42:40 +0700

    Bug SpotLight Atas Resolved

--- Assets/Resources/Adjuster/TopPointLightAdjuster.cs
+++ Assets/Resources/Adjuster/TopPointLightAdjuster.cs
@@ -5,10 +5,6 @@ using UnityEngine;
 
 public class TopPointLightAdjuster : MonoBehaviour
 {
-    bool isBackward = false;
-    float spinSpeed = 2.0f;
-    Vector3 RotateAmountX = new Vector3(50.0f, 0f, 0.0f);
-    Vector3 RotateAmountY = new Vector3(0.0f, 50.0f, 0.0f);
     Light lt;
     // Start is called before the first frame update
     void Start()
@@ -29,19 +25,5 @@ public class TopPointLightAdjuster : MonoBehaviour
     }
 
     void Update() {
-        if(isBackward == false){
-            transform.Rotate(RotateAmountX * Time.deltaTime / spinSpeed);
-        }      
-        else
-        {
-            transform.Rotate(RotateAmountX * -Time.deltaTime / spinSpeed);
-        }
-
-        if(UnityEditor.TransformUtils.GetInspectorRotation(gameObject.transform).x <= 45) {
-            isBackward = false;
-        }        
-        else if(UnityEditor.TransformUtils.GetInspectorRotation(gameObject.transform).x >= 150) {
-            isBackward = true;
-        }
     }
 }
--- Assets/Resources/Adjuster/TopSpotLightTubeAdjuster.cs
+++ Assets/Resources/Adjuster/TopSpotLightTubeAdjuster.cs
@@ -8,29 +8,55 @@ public class TopSpotLightTubeAdjuster : MonoBehaviour
     bool isBackward = false;
     float spinSpeed = 2f;
     Vector3 RotateAmountX = new Vector3(50.0f, 0f, 0.0f);
-    Vector3 TranslateAmountYZ = new Vector3(0.0f, 0.8f/2f, -1.2f/2f);
+    Vector3 TranslateAmountYZ1 = new Vector3(0.0f, 0.5f/2f, 1.1f/2f);
+    Vector3 TranslateAmountYZ2 = new Vector3(0.0f, 1.4f/2f, -0.1f/2f);
+    float positionY;
     // Start is called before the first frame update
     void Start()
     {
+        positionY = transform.position.y;
     }
 
     // Update is called once per frame
     void Update() {
-        if(isBackward == false){
-            transform.Rotate(RotateAmountX * Time.deltaTime / spinSpeed);
-            transform.Translate(TranslateAmountYZ * Time.deltaTime / spinSpeed);
-        }      
+        if(positionY > 13) {
+            if(isBackward == false){
+                transform.Rotate(RotateAmountX * Time.deltaTime / spinSpeed);
+                transform.Translate(TranslateAmountYZ1 * Time.deltaTime / spinSpeed);
+            }      
+            else
+            {
+                transform.Rotate(RotateAmountX * -Time.deltaTime / spinSpeed);
+                transform.Translate(TranslateAmountYZ1 * -Time.deltaTime / spinSpeed);
+            }
+
+            if(UnityEditor.TransformUtils.GetInspectorRotation(gameObject.transform).x <= 0) {
+                isBackward = false;
+                transform.position = new Vector3(transform.position.x, 13.03502f, 12.23554f);                
+            }        
+            else if(UnityEditor.TransformUtils.GetInspectorRotation(gameObject.transform).x >= 90) {
+                isBackward = true;
+            }
+        }
         else
         {
-            transform.Rotate(RotateAmountX * -Time.deltaTime / spinSpeed);
-            transform.Translate(TranslateAmountYZ * -Time.deltaTime / spinSpeed);
-        }
+            if(isBackward == true){
+                transform.Rotate(RotateAmountX * -Time.deltaTime / spinSpeed);
+                transform.Translate(TranslateAmountYZ2 * Time.deltaTime / spinSpeed);
+            }      
+            else
+            {
+                transform.Rotate(RotateAmountX * Time.deltaTime / spinSpeed);
+                transform.Translate(TranslateAmountYZ2 * -Time.deltaTime / spinSpeed);
+            }
 
-        if(UnityEditor.TransformUtils.GetInspectorRotation(gameObject.transform).x <= 120) {
-            isBackward = false;
-        }        
-        else if(UnityEditor.TransformUtils.GetInspectorRotation(gameObject.transform).x >= 225) {
-            isBackward = true;
+            if(UnityEditor.TransformUtils.GetInspectorRotation(gameObject.transform).x <= -90) {
+                isBackward = false;
+            }        
+            else if(UnityEditor.TransformUtils.GetInspectorRotation(gameObject.transform).x >= 0) {
+                isBackward = true;
+                transform.position = new Vector3(transform.position.x, 12.49679f, 13.15581f);
+            }
         }
     }
 }
