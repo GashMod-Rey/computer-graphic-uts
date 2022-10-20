f8536dae25ce45176e3c50b75962017ef8bfa127
GashMod-Rey <reynard.yaputra@student.umn.ac.id>
2022-10-20 16:03:37 +0700

    Terrain

--- Assets/Resources/Script/MyTerrain.cs
+++ Assets/Resources/Script/MyTerrain.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MyMonitorLayer : MonoBehaviour
+public class MyTerrain : MonoBehaviour
 {
     [SerializeField]
-    public Material chairMaterial;
+    public Material terrainMaterial;
     Texture myTexture;
 
     // Start is called before the first frame update
@@ -17,10 +17,10 @@ public class MyMonitorLayer : MonoBehaviour
         var uvs = new Vector2[vertices.Length];
 
         // Titik-titik tangkai monitor
-        vertices[0] = new Vector3(0, 2.25f, 0);
-        vertices[1] = new Vector3(9, 2.25f, 0);
-        vertices[2] = new Vector3(0, 6.25f, 0);
-        vertices[3] = new Vector3(9, 6.25f, 0);
+        vertices[0] = new Vector3(0, 2, 0);
+        vertices[1] = new Vector3(1000, 2, 0);
+        vertices[2] = new Vector3(0, 2, 1000);
+        vertices[3] = new Vector3(1000, 2, 1000);
 
         uvs[0] = new Vector2(1.0f, 0.0f);
         uvs[1] = new Vector2(0.0f, 0.0f);
@@ -30,14 +30,14 @@ public class MyMonitorLayer : MonoBehaviour
         mesh.vertices = vertices;
 
         mesh.triangles = new int[] {
-            0, 1, 2,
-            3, 2, 1,
+            2, 1, 0,
+            1, 2, 3,
         };
         mesh.RecalculateNormals();
         mesh.uv = uvs;
         GetComponent<MeshFilter>().mesh = mesh;
 
-        GetComponent<MeshRenderer>().material = chairMaterial;
+        GetComponent<MeshRenderer>().material = terrainMaterial;
     }
 
     // Update is called once per frame
